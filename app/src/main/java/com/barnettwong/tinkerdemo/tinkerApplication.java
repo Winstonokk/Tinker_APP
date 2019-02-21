4430d5e4a4d96118266e3ca8fdecf4d044c744cd
wangfeng19930909 <1300497680@qq.com>
2019-02-21 16:07:49 +0800

    适配androidstudio的gradle版本3.0+

--- app/src/main/java/com/barnettwong/tinkerdemo/tinkerApplication.java
+++ app/src/main/java/com/barnettwong/tinkerdemo/tinkerApplication.java
@@ -3,8 +3,11 @@ package com.barnettwong.tinkerdemo;
 
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.util.Log;
 
-import com.tencent.tinker.loader.app.ApplicationLike;
+import com.tencent.tinker.entry.ApplicationLike;
 import com.tinkerpatch.sdk.TinkerPatch;
 import com.tinkerpatch.sdk.loader.TinkerPatchApplicationLike;
 
@@ -17,22 +20,50 @@ import com.tinkerpatch.sdk.loader.TinkerPatchApplicationLike;
 public class tinkerApplication extends Application {
 
     private ApplicationLike tinkerApplicationLike;
+
+    public tinkerApplication() {
+
+    }
+
+    @Override
+    public void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        //在app/build.grade中配置multiDexEnabled true即可
+        //you must install multiDex whatever tinker is installed!
+        //MultiDex.install(base);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+        initTinkerPatch();
+    }
 
+    /**
+     * 我们需要确保至少对主进程跟patch进程初始化 TinkerPatch
+     */
+    private void initTinkerPatch() {
+        // 我们可以从这里获得Tinker加载过程的信息
         if (BuildConfig.TINKER_ENABLE) {
-            // 我们可以从这里获得Tinker加载过程的信息
             tinkerApplicationLike = TinkerPatchApplicationLike.getTinkerPatchApplicationLike();
-
-            // 初始化TinkerPatch SDK, 更多配置可参照API章节中的,初始化SDK
-            TinkerPatch.init(tinkerApplicationLike)
+            // 初始化TinkerPatch SDK
+            TinkerPatch.init(
+                    tinkerApplicationLike
+//                new TinkerPatch.Builder(tinkerApplicationLike)
+//                    .requestLoader(new OkHttp3Loader())
+//                    .build()
+            )
                     .reflectPatchLibrary()
                     .setPatchRollbackOnScreenOff(true)
-                    .setPatchRestartOnSrceenOff(true);
+                    .setPatchRestartOnSrceenOff(true)
+                    .setFetchPatchIntervalByHours(3)
+            ;
+            // 获取当前的补丁版本
+            Log.d("wang", "当前获取补丁：Current patch version is " + TinkerPatch.with().getPatchVersion());
 
-            // 每隔3个小时去访问后台时候有更新,通过handler实现轮训的效果
-            new FetchPatchHandler().fetchPatchWithInterval(3);
+            // fetchPatchUpdateAndPollWithInterval 与 fetchPatchUpdate(false)
+            // 不同的是，会通过handler的方式去轮询
+            TinkerPatch.with().fetchPatchUpdateAndPollWithInterval();
         }
     }
 }
