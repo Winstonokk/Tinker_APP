8a8b1aa483e6e5d1f56558a19620504b911f55d8
wangfeng19930909 <1300497680@qq.com>
2019-02-21 11:33:23 +0800

    热更新工程代码上传

---
+++ app/src/main/java/com/barnettwong/tinkerdemo/tinkerApplication.java
@@ -0,0 +1,38 @@
+package com.barnettwong.tinkerdemo;
+
+
+
+import android.app.Application;
+
+import com.tencent.tinker.loader.app.ApplicationLike;
+import com.tinkerpatch.sdk.TinkerPatch;
+import com.tinkerpatch.sdk.loader.TinkerPatchApplicationLike;
+
+
+/**
+ * Created by wang on 2019-2-20.
+ * reflectApplication = true 时
+ */
+
+public class tinkerApplication extends Application {
+
+    private ApplicationLike tinkerApplicationLike;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.TINKER_ENABLE) {
+            // 我们可以从这里获得Tinker加载过程的信息
+            tinkerApplicationLike = TinkerPatchApplicationLike.getTinkerPatchApplicationLike();
+
+            // 初始化TinkerPatch SDK, 更多配置可参照API章节中的,初始化SDK
+            TinkerPatch.init(tinkerApplicationLike)
+                    .reflectPatchLibrary()
+                    .setPatchRollbackOnScreenOff(true)
+                    .setPatchRestartOnSrceenOff(true);
+
+            // 每隔3个小时去访问后台时候有更新,通过handler实现轮训的效果
+            new FetchPatchHandler().fetchPatchWithInterval(3);
+        }
+    }
+}
